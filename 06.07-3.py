c8c785cee390096c7583ed7fa0a750c64bbbcbb2
YongWoo An <dyddn2015@gmail.com>
2024-06-08 23:48:24 +0900

    06.08

--- 06.07-3.py
+++ 06.07-3.py
@@ -1,23 +1,25 @@
 """
 백준 2470 두 용액
 
-1. 기본적으로 두 포인터 풀이
-2. 왼쪽 포인터를 두고, 오른쪽 포인터를 합의 절대값이 가장 작아질때까지 왼쪽으로 이동
-3. 해당 위치에서 오른쪽 포인터를 두고, 왼쪽 포인터만 한칸 오른쪽으로 이동
-4. 위 과정을 반복하며 절대값합이 가장 작아지는 경우를 찾아 tuple 저장
+1. 두 포인터 풀이
+2. 합의 최소 절대값을 업데이트하며 투포인터 알고리즘 진행
 """
 
 N = int(input())
 lst = sorted(map(int, input().split()))
 answer, answer_set = 2*10**9, (0, N-1)
 lp, rp= 0, N-1
-while lp<rp<N:
-    while lp<rp-1 and abs(lst[lp]+lst[rp-1])<=abs(lst[lp]+lst[rp]):
-        rp -= 1
-    if abs(answer)>abs(lst[lp]+lst[rp]):
-        answer = abs(lst[lp]+lst[rp])
+while lp<rp:
+    x = lst[lp]+lst[rp]
+    if answer>abs(x):
+        answer = abs(x)
         answer_set = (lst[lp], lst[rp])
-    lp += 1
+    if x>0:
+        rp -= 1
+    elif x<0:
+        lp += 1
+    else:
+        break
 print(*answer_set)
 
 
---
+++ 06.08.py
@@ -0,0 +1,32 @@
+"""
+백준 27172 수 나누기 게임
+
+1. 에라토스테네스의 체 원리와 비슷하게 풀이 진행
+2. card중 최대값을 찾아 이 값과 길이가 같은 scores라는 배열을 만들어 None으로 초기화
+3. 주어진 카드를 순회하며 scores[card값]의 원소를 0으로 초기화
+4. 다시 카드를 순회하는데, 값을 2/3/4..씩 곱해가며 배수에 해당하는 카드가 존재하면 -1점씩, 본인은 +1점씩 가져감
+5. 배수 계산은 카드의 max값까지만 진행
+"""
+input()
+cards = [*map(int, input().split())]
+max_card = max(cards)
+scores = [None]*(max_card+1)
+for card in cards:
+    scores[card] = 0
+for card in cards:
+    i = 2
+    while card*i<=max_card:
+        if scores[card*i] is not None:
+            scores[card*i] -= 1
+            scores[card] += 1
+        i+=1
+print(*[scores[card] for card in cards])
+
+
+
+
+
+
+
+
+
