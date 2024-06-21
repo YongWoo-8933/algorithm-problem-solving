b9c631035d07e36b0d144cae6cb0f79b9c561d8d
YongWoo An <dyddn2015@gmail.com>
2024-06-21 23:38:24 +0900

    06.21

---
+++ 06.21.py
@@ -0,0 +1,21 @@
+"""
+백준 16967 배열 복원하기
+
+1. 배열의 특징을 파악하고 복원
+"""
+from sys import stdin
+
+H, W, X, Y = map(int, stdin.readline().split())
+B = [[*map(int, i.split())] for i in stdin]
+A = [[0]*W for _ in range(H)]
+for row in range(H):
+    for col in range(W):
+        A[row][col] = B[row][col]
+for row in range(X, H+X):
+    for col in range(Y, W+Y):
+        if 0<=row<H and 0<=col<W:
+            A[row][col] -= A[row-X][col-Y]
+for i in range(H):
+    print(*A[i])
+
+
