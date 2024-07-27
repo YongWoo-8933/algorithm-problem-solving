c94ba73073085de0afb7acf4f9d8e9c48dc32a81
YongWoo An <dyddn2015@gmail.com>
2024-07-27 23:40:44 +0900

    07.27

--- 07.27.py
+++ 07.27.py
@@ -1,37 +1,15 @@
 """
 백준 4195 친구 네트워크 (골드2)
 
-민혁이는 소셜 네트워크 사이트에서 친구를 만드는 것을 좋아하는 친구이다. 
-우표를 모으는 취미가 있듯이, 민혁이는 소셜 네트워크 사이트에서 친구를 모으는 것이 취미이다.
+** union-find 유형에서 설정 3가지가 추가된 문제
+   1. 보통 node는 int로 주어지는데, 여기선 string(이름)으로 주어짐
+      => name_dict를 운영해 각 노드를 int로 변환
 
-어떤 사이트의 친구 관계가 생긴 순서대로 주어졌을 때, 두 사람의 친구 네트워크에 몇 명이 있는지 구하는 프로그램을 작성하시오.
+   2. 각 집합에 속하는 node 수를 구해야함
+      => cnt_dict를 운영해 union 함수가 실행될때마다 집합원 수를 업데이트
 
-친구 네트워크란 친구 관계만으로 이동할 수 있는 사이를 말한다.
-
-입력
-첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스의 첫째 줄에는 친구 관계의 수 F가 주어지며, 이 값은 100,000을 넘지 않는다. 
-다음 F개의 줄에는 친구 관계가 생긴 순서대로 주어진다. 친구 관계는 두 사용자의 아이디로 이루어져 있으며, 알파벳 대문자 또는 소문자로만 이루어진 길이 20 이하의 문자열이다.
-
-출력
-친구 관계가 생길 때마다, 두 사람의 친구 네트워크에 몇 명이 있는지 구하는 프로그램을 작성하시오.
-
-예제 입력 1 
-2
-3
-Fred Barney
-Barney Betty
-Betty Wilma
-3
-Fred Barney
-Betty Wilma
-Barney Betty
-예제 출력 1 
-2
-3
-4
-2
-2
-4
+   3. node의 수가 초기에 정해져있지 않음(동적임)
+      => name_dict를 통해 새로운 노드가 추가될때마다 parents 리스트를 갱신
 """
 from sys import stdin
 
@@ -42,41 +20,28 @@ def find(x: int) -> int:
     return parents[x]
 
 def union(a: int, b: int):
-    global parents
+    global parents, cnt_dict
     ra, rb = find(a), find(b)
     if ra!=rb:
         parents[max(ra, rb)] = min(ra, rb)
+        cnt_dict[min(ra, rb)] += cnt_dict[max(ra, rb)]
 
 input = stdin.readline
 
 for _ in range(int(input())):
     N = int(input())
-    name_dict = {}
     parents = []
-    dp_dict = {}
+    name_dict = {}
+    cnt_dict = {}
     idx = 0
     for __ in range(N):
         n1, n2 = input().strip().split()
-        # 두 이름이 이미 친구리스트에 있는경우
-        if n1 in name_dict and n2 in name_dict:
-            idx1, idx2 = name_dict[n1], name_dict[n2]
-            ra, rb = find(idx1), find(idx2)
-            if ra!=rb:
-                dp_dict[min(ra, rb)] += dp_dict[max(ra, rb)]
-            union(idx1, idx2)
-            print(dp_dict[min(ra, rb)])
-            continue
-        # 한 이름이라도 새로 추가된 경우
         for name in [n1, n2]:
             if name not in name_dict:
                 name_dict[name] = idx
+                cnt_dict[idx] = 1
                 parents.append(idx)
                 idx += 1
         idx1, idx2 = name_dict[n1], name_dict[n2]
         union(idx1, idx2)
-        root = find(idx1)
-        if root not in dp_dict:
-            dp_dict[root] = 2
-        else:
-            dp_dict[root] += 1
-        print(dp_dict[root])
+        print(cnt_dict[find(idx1)])
