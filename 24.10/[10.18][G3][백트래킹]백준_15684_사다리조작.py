b86eb62b77af49b8c5283fa9271914a4538d16b2
YongWoo An <dyddn2015@gmail.com>
2024-10-19 01:10:23 +0900

    10.18

--- 24.10/[10.18][G3][백트래킹]백준_15684_사다리조작.py
+++ 24.10/[10.18][G3][백트래킹]백준_15684_사다리조작.py
@@ -1,44 +1,44 @@
 """
 백준 15684 사다리 조작 (골드3)
 
-1. x좌표 기준 정렬 후 y값 비교하며 순회하면 됨
+** 극악의 백트래킹 문제 **
 """
 
 from sys import stdin
 
-def back_tracking(cnt: int, h: int, i: int, is_checked: bool) -> bool:
+def back_tracking(n: int, max_cnt: int):
     global ladder, answer, H, N
-    if cnt>=answer:
-        return False
-    elif h==H and i==N:
-        return False
-    elif i==N:
-        return back_tracking(cnt, h+1, 1, is_checked)
-    else:
-        if not is_checked and check():
-            answer = cnt
-            return True
-        if back_tracking(cnt, h, i+1, True):
-            return True
-        if ladder[h][i]==0 and ladder[h][i+1]==0:
-            ladder[h][i] = i+1
-            ladder[h][i+1] = i
-            result2 = back_tracking(cnt+1, h, i+1, False)
-            ladder[h][i] = 0
-            ladder[h][i+1] = 0
-            return result2
-        return False
 
-def check() -> bool:
+    if answer!=-1:
+        return
+    
+    cur_cnt = check()
+    if cur_cnt+(max_cnt-n)*2 < N:
+        return
+    
+    if n==max_cnt:
+        if cur_cnt==N:
+            answer = max_cnt
+        return
+    
+    for h in range(1, H+1):
+        for i in range(1, N):
+            if ladder[h][i]==0 and ladder[h][i+1]==0:
+                ladder[h][i], ladder[h][i+1] = i+1, i
+                back_tracking(n+1, max_cnt)
+                ladder[h][i], ladder[h][i+1] = 0, 0
+
+def check() -> int:
     global ladder, N, H
+    matched = 0
     for i in range(1, N+1):
         cur_i = i
         for h in range(1, H+1):
             if ladder[h][cur_i]!=0:
                 cur_i = ladder[h][cur_i]
-        if cur_i!=i:
-            return False
-    return True
+        if cur_i==i:
+            matched += 1
+    return matched
 
 N, M, H = map(int, stdin.readline().split())
 ladder = [[0]*(N+1) for _ in range(H+1)]
@@ -46,8 +46,9 @@ for i in stdin:
     a, b = map(int, i.split())
     ladder[a][b] = b+1
     ladder[a][b+1] = b
-answer = H*N
-if back_tracking(0, 1, 1, False):
-    print(answer)
-else:
-    print(-1)
+answer = -1
+for cnt in range(4):
+    back_tracking(0, cnt)
+    if answer != -1:
+        break
+print(answer)
