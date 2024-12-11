e8f21f0cb27f68337aa996fdaa15e2dec44abab3
YongWoo An <dyddn2015@gmail.com>
2024-12-11 23:32:33 +0900

    12.11

--- 24.10/[12.10][S1][DFS+DP]백준_1890_점프.py
+++ 24.10/[12.10][S1][DFS+DP]백준_1890_점프.py
@@ -1,14 +1,28 @@
 """
-백준 1309 동물원 (실버1)
+백준 1890 점프 (실버1)
 
 1. 동물원을 한줄일때부터 쭉 dp로 계산하면됨
 """
+from sys import stdin
 
 def main():
-    N = int(input())
-    left, right, none = 1, 1, 1
-    for _ in range(N-1):
-        left, right, none = (right+none)%9901, (left+none)%9901, (left+right+none)%9901
-    print((left+right+none)%9901)
+    N = int(stdin.readline())
+    MAP = [[*map(int, i.split())] for i in stdin]
+    dp = [[None]*N for _ in range(N)]
+    dp[N-1][N-1] = 1
+    
+    def dfs(row: int, col: int) -> int:
+        movable = MAP[row][col]
+        cnt = 0
+        for move in range(1, movable+1):
+            for nrow, ncol in [(row+move, col), (row, col+move)]:
+                if nrow<N and ncol<N:
+                    if dp[nrow][ncol] is None:
+                        dp[nrow][ncol] = dfs(nrow, ncol)
+                    cnt += dp[nrow][ncol]
+        return cnt
+    
+    print(dfs(0, 0))
+
 
 main()
