9c92575b056d9cf113be61fe7a91e570a381cc60
YongWoo An <dyddn2015@gmail.com>
2024-12-20 23:24:02 +0900

    12.20

--- 24.10/[12.20][G5][수학]백준_30704_정사각형연결하기.py
+++ 24.10/[12.20][G5][수학]백준_30704_정사각형연결하기.py
@@ -1,24 +1,20 @@
 """
 백준 30704 정사각형 연결하기 (골드5)
 
-1. combinations 패키지로 모든 수열 출력력
+1. 최대한 뭉치도록 계산
 """
 from sys import stdin
-
 
 def main():
     stdin.readline()
     for i in stdin:
         N = int(i)
         n = int(N**0.5)
-        if N-n<=n:
+        if N-n**2<1:
+            print(4*n)
+        elif N-n**2<n+1:
             print(4*n+2)
-        elif N-n<=2*n+1:
-            print(4*n+4)
-        elif N-n<=3*n+2:
-            print(4*n+6)
         else:
-            print(4*n+8)
+            print(4*n+4)
 
-        
 main()
